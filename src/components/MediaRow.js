948767eb1f704aeb62fa68fdac64a920fb05cdd7
Kasperi Helminen <kasperih@k-d557-mac-18.pk.amk.root>
2022-03-14 12:12:16 +0200

    ex1

---
+++ src/components/MediaRow.js
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+
+const MediaRow = ({file}) => {
+  return (
+    <tr>
+      <td>
+        <img src={file.thumbnails.w160} alt="kissa" />
+      </td>
+      <td>
+        <h4>{file.title}</h4>
+        <p>{file.description}</p>
+      </td>
+      <td>
+        <a href={file.filename}>View</a>
+      </td>
+    </tr>
+  );
+};
+
+MediaRow.propTypes = {
+  file: PropTypes.object.isRequired,
+};
+
+export default MediaRow;
