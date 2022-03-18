74b46fd2790538722c676112b13585a3dfdfb6eb
Kasperi Helminen <kasperih@k-d557-mac-18.pk.amk.root>
2022-03-18 15:10:36 +0200

    done

---
+++ src/hooks/ApiHooks.js
@@ -0,0 +1,27 @@
+import {useEffect, useState} from 'react';
+import {baseUrl} from '../utils/variables';
+
+const useMedia = () => {
+  const [mediaArray, setMediaArray] = useState([]);
+  const getMedia = async () => {
+    try {
+      const mediaResponse = await fetch(baseUrl + 'media');
+      const media = await mediaResponse.json();
+      const allFiles = await Promise.all(
+        media.map(async (file) => {
+          const fileResponse = await fetch(`${baseUrl}media/${file.file_id}`);
+          return await fileResponse.json();
+        })
+      );
+      setMediaArray(allFiles);
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+  useEffect(() => {
+    getMedia();
+  }, []);
+  return {mediaArray};
+};
+
+export {useMedia};
