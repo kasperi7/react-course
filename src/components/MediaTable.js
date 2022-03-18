ef18281bb7593ac033f091c5319f67db5abf04bc
Kasperi Helminen <kasperih@k-d557-mac-18.pk.amk.root>
2022-03-18 14:03:42 +0200

    done

--- src/components/MediaTable.js
+++ src/components/MediaTable.js
@@ -1,7 +1,21 @@
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
+import {useEffect, useState} from 'react';
 import MediaRow from './MediaRow';
 
-const MediaTable = ({mediaArray}) => {
+const MediaTable = () => {
+  const [mediaArray, setMediaArray] = useState([]);
+  const getMedia = async () => {
+    try {
+      const response = await fetch('test.json');
+      const json = await response.json();
+      setMediaArray(json);
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+  useEffect(() => {
+    getMedia();
+  }, []);
   console.log(mediaArray);
   return (
     <table>
@@ -14,8 +28,6 @@ const MediaTable = ({mediaArray}) => {
   );
 };
 
-MediaTable.propTypes = {
-  mediaArray: PropTypes.array.isRequired,
-};
+MediaTable.propTypes = {};
 
 export default MediaTable;
