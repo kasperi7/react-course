722a8d6631d5612a69083e8513ee48f7d61f57c1
Kasperi Helminen <kasperih@k-d557-mac-18.pk.amk.root>
2022-03-18 15:34:31 +0200

    done

--- src/hooks/ApiHooks.js
+++ src/hooks/ApiHooks.js
@@ -1,12 +1,25 @@
 import {useEffect, useState} from 'react';
 import {baseUrl} from '../utils/variables';
 
+const fetchJson = async (url, options = {}) => {
+  try {
+    const response = await fetch(url, options);
+    const json = await response.json();
+    if (response.ok) {
+      return json;
+    } else {
+      throw new Error('response error');
+    }
+  } catch (err) {
+    throw new Error(err.message);
+  }
+};
+
 const useMedia = () => {
   const [mediaArray, setMediaArray] = useState([]);
   const getMedia = async () => {
     try {
-      const mediaResponse = await fetch(baseUrl + 'media');
-      const media = await mediaResponse.json();
+      const media = await fetchJson(baseUrl + 'media');
       const allFiles = await Promise.all(
         media.map(async (file) => {
           const fileResponse = await fetch(`${baseUrl}media/${file.file_id}`);
